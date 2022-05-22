e40d03ec6eada16e62753b453d12a2559e22eb7d
X_O_R_S <Danbka220@yandex.ru>
2022-05-22 22:35:28 +0300

    <-

---
+++ Assets/Scripts/Core/SceneController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    private static Scene _currentScene;
+
+    public enum Scenes
+    {
+        Persistent,
+        Loading,
+        Menu,
+        V1
+    }
+
+    public async void LoadScene(Scenes scene)
+    {
+        
+        AsyncOperation operation = SceneManager.LoadSceneAsync(scene.ToString(),LoadSceneMode.Additive);
+        SceneManager.LoadSceneAsync(Scenes.Loading.ToString());
+
+        while (!operation.isDone)
+        {
+            await Task.Yield();
+        }
+
+        SceneManager.UnloadSceneAsync(_currentScene);
+        SceneManager.UnloadSceneAsync(Scenes.Loading.ToString());
+    }
+}
